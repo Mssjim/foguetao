60a13af8351d4e5412290a3e1ac5fcbfcddb04ac
Mssjim <mrmssjim@gmail.com>
2021-05-17 14:56:13 -0300

    Adição do Loop

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-console.log('------------');
 console.log('Hello World!');
-console.log('------------');
+
+timeout = (n) => {
+	setTimeout(() => {
+		console.log(n);
+	}, 3000);
+}
+
+for(let i=0; i<5; i++) {
+	timeout(i+1);
+}

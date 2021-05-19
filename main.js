e7e714c324222f72c4a30b08aa77b0e04e9e95c1
Mssjim <mrmssjim@gmail.com>
2021-05-19 18:24:00 -0300

    Refatoração apenas :pray:

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const { Client } = require('selfo.js');
 
-const bot = new Client();
-
-bot.on('ready', () => {
+new Client().on('ready', () => {
 	console.log(bot.user.tag + ' Online!');
-});
-
-bot.login(require('./token.json').token);
+}).login(require('./token.json').token);
